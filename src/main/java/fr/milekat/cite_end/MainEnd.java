4bb992ea512cd303960fdbcf9ba88f1748246c60
tutur1004 <arthur.barthelme78@gmail.com>
2020-12-10 17:22:06 +0100

    Initial commit

---
+++ src/main/java/fr/milekat/cite_end/MainEnd.java
@@ -0,0 +1,15 @@
+package fr.milekat.cite_end;
+
+import org.bukkit.plugin.java.JavaPlugin;
+
+public class MainEnd extends JavaPlugin {
+    @Override
+    public void onEnable() {
+
+    }
+
+    @Override
+    public void onDisable() {
+
+    }
+}
