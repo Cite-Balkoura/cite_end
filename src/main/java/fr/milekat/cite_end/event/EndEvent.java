43d0124145a6ca951890377de50949dba7277dee
tutur1004 <arthur.barthelme78@gmail.com>
2020-12-15 23:59:28 +0100

    - Get last survie

--- src/main/java/fr/milekat/cite_end/event/EndEvent.java
+++ src/main/java/fr/milekat/cite_end/event/EndEvent.java
@@ -3,6 +3,7 @@ package fr.milekat.cite_end.event;
 import fr.milekat.cite_core.MainCore;
 import fr.milekat.cite_core.core.bungee.ServersManagerSendPlayer;
 import fr.milekat.cite_end.MainEnd;
+import fr.milekat.cite_libs.MainLibs;
 import org.bukkit.Bukkit;
 import org.bukkit.Location;
 import org.bukkit.entity.Player;
@@ -10,9 +11,37 @@ import org.bukkit.event.EventHandler;
 import org.bukkit.event.Listener;
 import org.bukkit.event.entity.EntityDamageByBlockEvent;
 import org.bukkit.event.entity.EntityDamageByEntityEvent;
+import org.bukkit.event.player.PlayerJoinEvent;
 import org.bukkit.event.player.PlayerMoveEvent;
 
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.SQLException;
+
 public class EndEvent implements Listener {
+    @EventHandler
+    public void onJoin(PlayerJoinEvent event) {
+        if (event.getPlayer().getLocation().getY() <= 0) {
+            event.getPlayer().teleport(MainCore.locLabels.getOrDefault("end_spawn",
+                    new Location(Bukkit.getWorld("world_the_end"),0,150,0)));
+        }
+        try {
+            Connection connection = MainLibs.getSql();
+            PreparedStatement q = connection.prepareStatement(
+                    "SELECT COALESCE(`server`, 'survie_prague') as server FROM `balkoura_last_survie` WHERE `uuid` = ?;");
+            q.setString(1, event.getPlayer().getUniqueId().toString());
+            q.execute();
+            if (q.getResultSet().last()) {
+                MainEnd.lastPlayerSurvie.put(event.getPlayer(), q.getResultSet().getString("server"));
+            } else {
+                MainEnd.lastPlayerSurvie.put(event.getPlayer(), "survie_prague");
+            }
+            q.close();
+        } catch (SQLException throwables) {
+            throwables.printStackTrace();
+        }
+    }
+
     @EventHandler
     public void onVoidFell(PlayerMoveEvent event) {
         if (event.getTo()!=null && event.getTo().getY()<=0) {
@@ -20,6 +49,18 @@ public class EndEvent implements Listener {
             location.setY(250);
             new ServersManagerSendPlayer().sendPlayerToServer(event.getPlayer(),
                     MainEnd.lastPlayerSurvie.getOrDefault(event.getPlayer(),"Prague"),"world",location);
+            event.getPlayer().teleport(MainCore.locLabels.getOrDefault("survie_end_spawn",
+                    new Location(Bukkit.getWorld("world_the_end"),0,150,0)));
+        }
+    }
+
+    @EventHandler
+    public void onMoveOverworld(PlayerMoveEvent event) {
+        if (event.getTo()!=null && event.getTo().getWorld()!=null &&
+                !event.getTo().getWorld().getName().equalsIgnoreCase("world_the_end")) {
+            Location location = event.getTo().clone();
+            location.setWorld(Bukkit.getWorld("world_the_end"));
+            event.getPlayer().teleport(location);
         }
     }
 
@@ -44,9 +85,11 @@ public class EndEvent implements Listener {
     }
 
     private void onEndDeath(Player player) {
-        player.teleport(MainCore.locLabels.getOrDefault("end_spawn",
+        player.teleport(MainCore.locLabels.getOrDefault("survie_end_spawn",
                 new Location(Bukkit.getWorld("world_the_end"),0,150,0)));
+        player.setHealth(20.0);
+        player.setFoodLevel(20);
         new ServersManagerSendPlayer().sendPlayerToServer(player,
-                MainEnd.lastPlayerSurvie.getOrDefault(player,"Prague"),"respawn");
+                MainEnd.lastPlayerSurvie.getOrDefault(player,"survie_prague"),"respawn");
     }
 }
